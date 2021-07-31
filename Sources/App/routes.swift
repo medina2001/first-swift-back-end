945b0a9db66e804c463b2283dc7c5832d816aa3c
Gabriel Maciel <gabrielmaciel2310@gmail.com>
2021-07-31 02:21:48 -0400

    Hello World!

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,6 +4,11 @@ func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
     }
+    
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, I'm \(name)!"
+    }
 
     app.get("hello") { req -> String in
         return "Hello, world!"
